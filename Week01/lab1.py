4297fae8db7971c4d415ddb64ca3bc3248f715df
hannahovarb <hannahovarb@gmail.com>
2026-02-02 16:57:54 -0500

    Starting week 02 Lab

---
+++ Week01/lab1.py
@@ -0,0 +1,22 @@
+# Sample Coding Question 01 Week 01
+# Hannah Bravo
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3:
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Question 4:
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: ", userAge)
